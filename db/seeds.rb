9cb6c74bf6e0f3cb2d1666e74b2fafaeba67ffec
Wouter Hilderson <wouter.hilderson@gmail.com>
2022-01-18 22:03:22 +0100

    added routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+# remove old items
+puts 'cleaning database'
+Restaurant.destroy_all
+
+puts 'creating restaurants'
+
+10.times do
+  restaurant = Restaurant.create(name: Faker::FunnyName.name,
+                              address: Faker::Address.full_address,
+                              phone_number: Faker::PhoneNumber.phone_number,
+                              category: %w[chinese italian japanese french belgian].sample)
+  puts "created #{restaurant.name}"
+end
